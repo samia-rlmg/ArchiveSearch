148c67de752f47a33a228d48c65551f531ab4a67
Samia Bouzid <samia@Samias-MacBook-Pro.local>
2019-03-12 17:50:43 -0400

    Consolidated all search fields into one. Removed bootstrap so that CSS column resizing would work.

--- Google Spreadsheet Query/js/main.js
+++ Google Spreadsheet Query/js/main.js
@@ -6,13 +6,22 @@ google.charts.load('current', {'packages':['table']});
 //Set Google Sheets unique key
 var gsKey = "1LST9-FQj7HKwGB3TyyaUay4fMpS7-B4_0fafC_EOGlY";
 
+$("input").keyup(function(event) {
+  if (event.keyCode === 13) {
+    $("#btn").click();
+  }
+});
+
+// $('input').on('keypress', (event) ==> {
+//   if(event.which === 13) {
+//     $("#btn").click();
+//   }
+// })
+
 //Use user-entered search terms to define search variables
 function defineSearch() {
   //Set variables based on user input
-  var driveNo = $("#driveNo").val();
-  var project = $("#project").val();
-  var keywords = $("#keywords").val();
-  var producer = $("#producer-editor").val();
+  var searchTerms = $("#search-terms").val();
   var andOperator = $("#andLogic").is(":checked");
   var orOperator = $("#orLogic").is(":checked");
 
@@ -30,48 +39,41 @@ function defineSearch() {
   console.log("Operator: "+operator);
 
   //Call the function that will formulate a query for Google Sheets
-  formulateQuery(driveNo, project, keywords, producer, operator);
+  formulateQuery(searchTerms, operator);
 
 }
 
 //Formulate a query for Google Sheets based on user-entered search terms
-function formulateQuery(driveNo, project, keywords, producer, operator) {
+function formulateQuery(searchTerms, operator) {
 
     //Initialize a string that will hold the query sent to Google Sheets
     var queryArr = [];
 
-    //If a project search term is set, add it to the query.
-    if (driveNo) {
-      queryArr.push("(lower(A) contains lower('"+driveNo+"'))");
-    }
+    //If search terms are set, add them all to the query.
+    if (searchTerms) {
+      var searchTermArr = searchTerms.split(',');
+      searchTermArr = trimArray(searchTermArr);
 
-    if (project) {
-      queryArr.push("(lower(B) contains lower('"+project+"'))");
-    }
+      var subQueryArr = [];
+      for (i=0;i<searchTermArr.length;i++) {
+        // NOTE: Exclude any columns with dates or other non-text formats\
+        subQueryArr.push("lower(A) contains lower('"+searchTermArr[i]+"')");
+        subQueryArr.push("lower(B) contains lower('"+searchTermArr[i]+"')");
+        subQueryArr.push("lower(C) contains lower('"+searchTermArr[i]+"')");
+        subQueryArr.push("lower(O) contains lower('"+searchTermArr[i]+"')");
+        subQueryArr.push("lower(J) contains lower('"+searchTermArr[i]+"')");
 
-    //If keywords and a project are set, add them all to the query.
-    if (keywords) {
-      var keywordArr = keywords.split(',');
-      keywordArr = trimArray(keywordArr);
-
-      for (i=0;i<keywordArr.length;i++) {
-        queryArr.push("(lower(J) contains lower('"+keywordArr[i]+"')) ");
+      if (subQueryArr) {
+        queryArr.push(subQueryArr.join(" OR "));
       }
     }
 
-    //If a producer and either project or keywords are set, add it to the query.
-    if (producer) {
-      queryArr.push("(lower(O) contains lower('"+producer+"'))");
-    }
-
-    if (queryArr) {
-      console.log("Query pieces: "+queryArr.join(" "+operator+" "));
     }
 
     var gsQuery;
 
     //If no search terms are set, print a log message.
-    if (!(driveNo) && !(project) && !(keywords) && !(producer)) {
+    if (!searchTerms) {
       console.log("No search terms set");
       gsQuery = "SELECT *";
     } else {
@@ -97,104 +99,22 @@ function handleQueryResponse(response) {
   }
 
   var data = response.getDataTable();
-  var dataClean = JSON.stringify(data, null, 4).replace(/\\/g, "");
-  var dataClean = `{"cols":
-  [
-      {"id":"A",
-      "label":"Hard Drive Number",
-      "type":"string"},
-      {"id":"B","label":"Project","type":"string"},
-      {"id":"C","label":"Format","type":"string"},
-      {"id":"D","label":"Pixel Size","type":"string"},
-      {"id":"E","label":"Start Date","type":"date","pattern":"m/d/yyyy"},
-      {"id":"F","label":"End Date","type":"date","pattern":"m/d/yyyy"},
-      {"id":"G","label":"Editing Version","type":"string"},
-      {"id":"H","label":"File Folder Name","type":"string"},
-      {"id":"I","label":"Final File Format","type":"string"},
-      {"id":"J","label":"Keywords","type":"string"},
-      {"id":"K","label":"Location of Files","type":"string"},
-      {"id":"L","label":"LTO Tape","type":"string"},
-      {"id":"M","label":"Notes","type":"string"},
-      {"id":"N","label":"Original File Format","type":"string"},
-      {"id":"O","label":"Producer/Editor","type":"string"},
-      {"id":"P","label":"Raw Footage","type":"string"},
-      {"id":"Q","label":"Description of Raw Footage","type":"string"},
-      {"id":"R","label":"Preview Link","type":"string"}
-    ],
-
-  "rows":
-    [
-      {"v":"CAL 001"},
-      {"v":"Denver Museum of Nature and Science"},
-      {"v":"JPEG"},
-      {"v":"LARGE"},
-      {"v":"Date(2010, 5, 28)","f":"6/28/2010"},
-      {"v":"Date(2013, 8, 12)","f":"9/12/2013"},
-      null,
-      {"v":"Hi Res Still at DMNS"},
-      null,
-      null,
-      null,
-      {"v":"LTO 001"},
-      {"v":"Publicity stills taken by Design Firm of final installation"},
-      null,
-      null,
-      null,
-      null,
-      {"v":"preview"}
-    ],
-    [
-      {"v":"CAL 001"},
-      {"v":"Denver Museum of Nature and Science"},
-      {"v":"JPEG"},
-      {"v":"LARGE"},
-      {"v":"Date(2010, 5, 28)","f":"6/28/2010"},
-      {"v":"Date(2013, 8, 12)","f":"9/12/2013"},
-      null,
-      {"v":"Hi Res Still at DMNS"},
-      null,
-      null,
-      null,
-      {"v":"LTO 001"},
-      {"v":"Publicity stills taken by Design Firm of final installation"},
-      null,
-      null,
-      null,
-      null,
-      {"v":"preview"}
-    ]
-  }`
-  //var dataObj = JSON.parse(dataClean);
-  //console.log(dataClean);
-
-  //This is for making a table without Google visualization; can cut
-
-  // var keys = Object.keys(data);
-  // console.log("Keys: " + Object.keys(dataObj));
-  // var col = [];
-  //   for (var i = 0; i < keys.length; i++) {
-  //     console.log(JSON.stringify(dataObj));
-  //       for (var key in keys[i]) {
-  //         if (col.indexOf(key) === -1) {
-  //           col.push(key);
-  //         }
-  //       }
-  //   }
-  // console.log("Columns: " + col);
-
-  //end cuttable section
 
   var cssClassNames = {tableCell: 'table-cell'};
   var options = {'cssClassNames': cssClassNames, 'showRowNumber': false, 'width': '100%', 'allowHtml': true};
 
-  var hdTable = new google.visualization.Table(document.getElementById('showData'));
+  if ((JSON.stringify(data)).includes('[]')) {
+    alert("No entries match your search.")
+  } else {
+    var hdTable = new google.visualization.Table(document.getElementById('showData'));
 
-  var formatter = new google.visualization.PatternFormat();
+    var formatter = new google.visualization.PatternFormat();
 
-  data.setProperty(2,9,'style','width:400px');
+    data.setProperty(0,9,'style','width:400px');
 
-  hdTable.draw(data, options);
-  $( ".table-cell" ).wrapInner( "<div class='table-div'></div>");
+    hdTable.draw(data, options);
+    $( ".table-cell" ).wrapInner( "<div class='table-div'></div>");
+  }
 }
 
 function trimArray(arr) {
