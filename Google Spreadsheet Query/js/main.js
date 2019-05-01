d3b1c0844f3785c3b279b16fc886f5ea21db90bc
Samia Bouzid <samia@Samias-MacBook-Pro.local>
2019-05-01 17:43:16 -0400

    Reformatted according to Nicole's design

--- Google Spreadsheet Query/js/main.js
+++ Google Spreadsheet Query/js/main.js
@@ -9,206 +9,198 @@ var gsKey = "1LST9-FQj7HKwGB3TyyaUay4fMpS7-B4_0fafC_EOGlY";
 // Make "Enter" trigger the submit button
 $("input").keyup(function(event) {
   if (event.keyCode === 13) {
-    $("#btn").click();
+    $("#submit").click();
   }
 });
+
+// Get entries from Google sheet when page loads or when user clicks "Submit"
+window.addEventListener("load", getJSON);
+document.getElementById("submit").addEventListener("click", getJSON);
 
 //Use user-entered search terms to define search variables
 function defineSearch() {
   //Set variables based on user input
-  var searchTerms = $("#search-terms").val();
+  var searchTerms = $("#search-bar").val();
   var searchTermArr = searchTerms.split(',');
   searchTermArr = trimArray(searchTermArr);
-  getJSON(searchTermArr);
-
+  return searchTermArr;
 }
 
-function getJSON(searchTermArr) {
+function getJSON() {
 
   // Get spreadsheet data in JSON format
   const xhr = new XMLHttpRequest();
   const url='https://spreadsheets.google.com/feeds/list/'+gsKey+'/1/public/full?alt=json';
   xhr.open("GET", url);
   xhr.send();
-  xhr.onreadystatechange=(e)=>{
-    var sheetObj = JSON.parse(xhr.responseText);
-    makeTable(sheetObj);
-
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      var sheetObj = JSON.parse(xhr.responseText);
+      makeTable(sheetObj); //Call the function that formats this JSON into a table
+    }
   }
+}
 
-  function makeTable(jsObj) {
-    // Header row
-    var txt = "<table id='archive-entries'><tr id='table-title'><td>ENTRIES</td><td></td><td></td><td></td></tr><tr><td><br></td></tr>";
-    var content;
+function makeTable(jsObj) {
 
-    // Fill in table contents
-    for (i=0; i<jsObj.feed.entry.length; i++) {
-      content += jsObj.feed.entry[i].gsx$projectname.$t;
-      content += jsObj.feed.entry[i].gsx$drivenumber.$t;
-      content += jsObj.feed.entry[i].gsx$companycode.$t;
-      content += jsObj.feed.entry[i].gsx$ltonumber.$t;
-      content += jsObj.feed.entry[i].gsx$drivecontents.$t;
-      content += jsObj.feed.entry[i].gsx$projectmanagersproducers.$t;
-      content += jsObj.feed.entry[i].gsx$featuredexhibits.$t;
+  var searchTermArr = defineSearch();
 
-      var hit = 0;
-      for (j=0; j<searchTermArr.length; j++) {
-        if (content.includes(searchTermArr[j])) {
-          hit = 1;
-        }
+  // Header row
+  var txt = "<table id='archive-entries'><tr id='table-title'><td>ENTRIES</td><td></td><td></td><td></td></tr><tr><td><br></td></tr>";
+  var content;
+
+  // Fill in table contents
+  for (i=0; i<jsObj.feed.entry.length; i++) {
+    //Add each field in the JSON object to a string. Make it lower case for searching later.
+    content += " " + jsObj.feed.entry[i].gsx$projectname.$t.toLowerCase();
+    content += " " + jsObj.feed.entry[i].gsx$drivenumber.$t.toLowerCase();
+    content += " " + jsObj.feed.entry[i].gsx$companycode.$t.toLowerCase();
+    content += " " + jsObj.feed.entry[i].gsx$ltonumber.$t.toLowerCase();
+    content += " " + jsObj.feed.entry[i].gsx$drivecontents.$t.toLowerCase();
+    content += " " + jsObj.feed.entry[i].gsx$projectmanagersproducers.$t.toLowerCase();
+    content += " " + jsObj.feed.entry[i].gsx$featuredexhibits.$t.toLowerCase();
+
+    // Turn content into an array where each word is its own element.
+    //(This will also break up multi-word fields into separate elements.)
+    content = content.split(" ");
+
+    var hit = 0;
+    for (j=0; j<searchTermArr.length; j++) {
+
+      if (content.indexOf(searchTermArr[j].toLowerCase()) > -1) {
+        hit = 1;
+      }
+    }
+
+    // Make a new table row if this entry contains a search term or if no serach terms were entered.
+    if (hit == 1 || searchTermArr == "") {
+        // First row
+        txt += "<tbody class='table-entry'><tr>" +
+            "<td><span class='project-name'>"+jsObj.feed.entry[i].gsx$projectname.$t+"</span>" +
+            "<span class='drive-no'> | Drive #"+jsObj.feed.entry[i].gsx$drivenumber.$t+"</span></td>" +
+            "<td></td><td></td>" +
+            "<td class='company-code'>"+jsObj.feed.entry[i].gsx$companycode.$t+"</td></tr></div>" +
+
+        // Second row
+            "<tr><td><span>"+jsObj.feed.entry[i].gsx$projectstartdate.$t+" – </span>" +
+            "<span>"+jsObj.feed.entry[i].gsx$projectenddate.$t+"</span></td>" +
+            "<td></td><td></td>" +
+            "<td class='lto-no'>LTO Tape " + jsObj.feed.entry[i].gsx$ltonumber.$t + "</td></tr>" +
+
+        // Third row
+            "<tr><td><a href="+jsObj.feed.entry[i].gsx$vimeoalbumlink.$t+">"+jsObj.feed.entry[i].gsx$vimeoalbumlink.$t+"</a></td></tr>" +
+            "<tr><td><br></td></tr>" +
+
+        // Fourth row
+            "<tr><td class='heading'>Drive Contents</td>" +
+            "<td class='heading'>Project Specs</td>" +
+            "<td class='heading'>Project Manager(s) / Producer(s)</td>" +
+            "<td class='heading'>Featured Exhibits</td></tr>" +
+
+        // Fifth row
+            "<tr><td>"+jsObj.feed.entry[i].gsx$drivecontents.$t+"</td>" +
+            "<td>"+jsObj.feed.entry[i].gsx$projectspecs.$t+"</td>" +
+            "<td>"+jsObj.feed.entry[i].gsx$projectmanagersproducers.$t+"</td>" +
+            "<td>"+jsObj.feed.entry[i].gsx$featuredexhibits.$t+"</td></tr>" +
+            "<tr><td><br></td><td></td><td></td><td></td></tr>" +
+            "<tr class='last-row'><td><br></td><td></td><td></td><td></td></tr>" +
+            "</tr></tbody>";
       }
 
-      if (hit == 1 || searchTermArr == []) {
-          // First row
-          txt += "<div class='table-entry'><tr>";
-          txt += "<td><span class='project-name'>"+jsObj.feed.entry[i].gsx$projectname.$t+"</span>";
-          txt += "<span class='drive-no'> | Drive #"+jsObj.feed.entry[i].gsx$drivenumber.$t+"</span></td>";
-          txt += "<td></td><td></td>"
-          txt += "<td class='company-code'>"+jsObj.feed.entry[i].gsx$companycode.$t+"</td></tr></div>";
+    }
 
-          // Second row
-          txt += "<tr><td><span>"+jsObj.feed.entry[i].gsx$projectstartdate.$t+" – </span>";
-          txt += "<span>"+jsObj.feed.entry[i].gsx$projectenddate.$t+"</span></td>";
-          txt += "<td></td><td></td>"
-          txt += "<td class='lto-no'>LTO Tape " + jsObj.feed.entry[i].gsx$ltonumber.$t + "</td></tr>"
+  txt += "</table>";
+  document.getElementById("showData").innerHTML = txt;
+}
 
-          // Third row
-          txt += "<tr><td><a href="+jsObj.feed.entry[i].gsx$vimeoalbumlink.$t+">"+jsObj.feed.entry[i].gsx$vimeoalbumlink.$t+"</a></td></tr>";
-          txt += "<tr><td><br></td></tr>"
+//When the user clicks on the button, toggle between hiding and showing the dropdown content
+document.getElementById("dropbtn").addEventListener("click", toggle);
 
-          // Fourth row
-          txt += "<tr><td class='heading'>Drive Contents</td>"
-          txt += "<td class='heading'>Project Specs</td>"
-          txt += "<td class='heading'>Project Manager(s) / Producer(s)</td>"
-          txt += "<td class='heading'>Featured Exhibits</td></tr>"
+function toggle() {
+  document.getElementById("sort-dropdown").classList.toggle("show");
+}
 
-          // Fifth row
-          txt += "<tr><td>"+jsObj.feed.entry[i].gsx$drivecontents.$t+"</td>";
-          txt += "<td>"+jsObj.feed.entry[i].gsx$projectspecs.$t+"</td>";
-          txt += "<td>"+jsObj.feed.entry[i].gsx$projectmanagersproducers.$t+"</td>";
-          txt += "<td>"+jsObj.feed.entry[i].gsx$featuredexhibits.$t+"</td></tr>";
-          txt += "<tr><td><br></td><td></td><td></td><td></td></tr>"
-          txt += "<tr class='last-row'><td><br></td><td></td><td></td><td></td></tr>"
-          txt += "</tr></div>";
-        }
-
-      }
-
-    txt += "</table>";
-    document.getElementById("showData").innerHTML = txt;
-  }
-
-  //When the user clicks on the button, toggle between hiding and showing the dropdown content
-  document.getElementById("dropbtn").addEventListener("click", toggle);
-
-  function toggle() {
-    document.getElementById("sortDropdown").classList.toggle("show");
-  }
-
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('#dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('#dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
 
-  document.getElementById("sort-project").addEventListener("click", function() {
-    sortTable("sort-project");
-  });
+document.getElementById("sort-project").addEventListener("click", function() {
+  sortTable("project-name");
+});
+document.getElementById("sort-drive").addEventListener("click", function() {
+  sortTable("drive-no");
+});
+document.getElementById("sort-lto").addEventListener("click", function() {
+  sortTable("lto-no");
+});
 
-  function sortTable(sortOn) {
-    console.log("Made it to sortTable");
-    var table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("archive-entries");
-    switching = true;
-    /* Make a loop that will continue until
-    no switching has been done: */
-    while (switching) {
-      // Start by saying: no switching is done:
-      switching = false;
-      rows = document.getElementsByClassName('table-entry');
-      console.log(rows[i]);
-      /* Loop through all table rows (except the
-      first, which contains table headers): */
-      for (i = 1; i < (rows.length - 1); i++) {
-        // Start by saying there should be no switching:
-        shouldSwitch = false;
-        /* Get the two elements you want to compare,
-        one from current row and one from the next: */
-        console.log(rows[i].getElementsByTagName("TD"));
-        x = rows[i].getElementsByTagName("TD")[0];
-        y = rows[i + 1].getElementsByTagName("TD")[0];
-        //xval = rows[i].getElementById(sortOn);
-        console.log("rows[i]: "+rows[i].getElementsByTagName("TD")[0]);
-        //yval = rows[i + 1].getElementById(sortOn);
-        // Check if the two rows should switch place:
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
+function sortTable(sortOn) {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("archive-entries");
+  switching = true;
+
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = document.getElementsByClassName('table-entry');
+    /* Loop through all archive entries: */
+    for (i = 0; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = $(".table-entry:eq(" + i + ") ." + sortOn).text();
+      y = $(".table-entry:eq(" + (i+1) + ") ." + sortOn).text();
+
+      // Clean up x and y depending on the sort value
+      if (sortOn == "drive-no") {
+        x = parseInt(x.split("#")[1]);
+        y = parseInt(y.split("#")[1]);
       }
-      if (shouldSwitch) {
-        /* If a switch has been marked, make the switch
-        and mark that a switch has been done: */
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
+
+      if (sortOn == "lto-no") {
+        x = parseInt(x.split(" ")[2]);
+        y = parseInt(y.split(" ")[2]);
       }
+
+      if (sortOn == "project-no") {
+        x = x.toLowerCase();
+        y = y.toLowerCase();
+      }
+
+      // Check if the two rows should switch place:
+      if (x > y) {
+        // If so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i+1], rows[i]);
+      switching = true;
+    }
   }
 }
-
-}
-//     var query = new google.visualization.Query(
-//           'http://spreadsheets.google.com/tq?key='+gsKey+'&gid=0');
-//
-//       // Apply query language.
-//       query.setQuery(gsQuery);
-//
-//       // Send the query with a callback function.
-//       query.send(handleQueryResponse);
-//   }
-//
-// function handleQueryResponse(response) {
-//   if (response.isError()) {
-//     console.log ("Error sending query");
-//     alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-//     return;
-//   }
-//
-//   var data = response.getDataTable();
-//
-//   // Add a class (table-cell) to each table cell so they can be accessed directly
-//   var cssClassNames = {tableCell: 'table-cell'};
-//   var options = {'cssClassNames': cssClassNames, 'showRowNumber': false, 'width': '100%', 'allowHtml': true};
-//
-//   // Only proceed if the search returns at least one row
-//   if ((JSON.stringify(data)).includes('[]')) { //Any search with no results will return an empty array of rows
-//     alert("No entries match your search.")
-//   } else {
-//     var hdTable = new google.visualization.Table(document.getElementById('showData'));
-//
-//     // var formatter = new google.visualization.PatternFormat(); //Can use this to format content of cells
-//
-//     //data.setProperty(0,9,'style','width:400px'); //This appears to be getting overridden
-//
-//     // Create a table with the returned rows
-//     hdTable.draw(data, options);
-//     $( ".table-cell" ).wrapInner( "<div class='table-div'></div>"); //Wrap them in a div with a specific class so they can be formatted directly
-//   }
-// }
 
 // Trim the leading and trailing white space out of each array item
 function trimArray(arr) {
   for(i=0;i<arr.length;i++)
   {
-      arr[i] = arr[i].replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+    arr[i] = arr[i].replace(/^\s\s*/, '').replace(/\s\s*$/, '');
   }
   return arr;
 }
